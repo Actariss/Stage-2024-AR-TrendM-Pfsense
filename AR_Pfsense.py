35b02272f755921d7c04b5432a62bd94c6be2059
Actariss <104220453+Actariss@users.noreply.github.com>
2024-05-13 09:50:02 +0200

    Update AR_Pfsense.py, command is now blocking on OPT1

--- AR_Pfsense.py
+++ AR_Pfsense.py
@@ -130,7 +130,7 @@ def main():
     try:
         new_dict = {
             "type": "block",
-            "interface": "wan",
+            "interface": "opt1",
             "ipprotocol": "inet",
             "protocol": "any",
             "src": sys.argv[1],
